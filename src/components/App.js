89d865b24de1ab6a53e9010addff791aaf7130c7
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-19 21:00:31 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
+import { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(1);
+
+  const increaseCount=()=>{
+    setCount(count+1);
+
+    let span = document.getElementById("number");
+    span.innerHTML = count;
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked <span id="number">0</span> times</p>
+        <button id="btn" onClick={increaseCount}>CLick Me</button>
     </div>
   )
 }
